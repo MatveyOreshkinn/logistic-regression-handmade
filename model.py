42ed21ccb870ef8ecc9fae5b7dbad4a1b14cb559
Oreshkin Matway <matveyoreshkin2005gmail.com>
2025-01-01 13:13:05 +0500

    add predict and predict_proba

--- model.py
+++ model.py
@@ -18,6 +18,7 @@ class MyLogReg:
         return f'MyLogReg class: n_iter={self.n_iter}, learning_rate={self.learning_rate}'
 
     def fit(self, X: pd.DataFrame, y: pd.Series, verbose: Optional[bool] = False) -> None:
+        X = X.copy()  # чтобы не менять оригинальный dataframe
         X.insert(0, 'x0', 1)  # оптимизация для нахождения градиента
         cnt_features = X.shape[1]
 
@@ -26,19 +27,29 @@ class MyLogReg:
 
         for i in range(self.n_iter):
             y_pred = np.dot(X, self.weights)
-            func = np.vectorize(sigmoid)
-            y_prob = func(y_pred)
+            y_prob = sigmoid(y_pred)
 
             # Добавляем небольшое значение для предотвращения log(0)
             epsilon = 1e-15
-            logloss = -np.mean(y * np.log(y_prob + epsilon) + (1 - y)
-                               * np.log(1 - y_prob + epsilon))
+            y_prob = np.clip(y_prob, epsilon, 1 - epsilon)
+            logloss = -np.mean(y * np.log(y_prob) + (1 - y)
+                               * np.log(1 - y_prob))
 
             grad = (y_prob - y) @ X / X.shape[0]
             self.weights -= self.learning_rate * grad
 
             if verbose and i % verbose == 0:
                 print(f'{i} | loss: {logloss:.2f}')
+
+    def predict(self, X: pd.DataFrame) -> np.ndarray:
+        y_prob = self.predict_proba(X)
+        return np.where(y_prob > 0.5, 1, 0)
+
+    def predict_proba(self, X: pd.DataFrame) -> np.ndarray:
+        X = X.copy()  # чтобы не менять оригинальный dataframe
+        X.insert(0, 'x0', 1)
+        y_pred = np.dot(X, self.weights)
+        return sigmoid(y_pred)
 
     def get_coef(self) -> np.ndarray:
         """
